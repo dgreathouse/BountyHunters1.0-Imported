3b69d500b9b8a1f8ab44d63018f879410e95c2c8
Sam <28huiskenss@e-hps.net>
2024-01-19 16:08:35 -0500

    Me

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -2,6 +2,8 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
@@ -9,16 +11,25 @@ import frc.robot.lib.ICommand;
 import frc.robot.lib.ISubsystem;
 
 public class ShooterSubsystem extends SubsystemBase implements ISubsystem {
+  TalonFX m_leftMotor;
+  TalonFX m_rightMotor;
   public void updateDashboard() {
     if(this.getCurrentCommand() != null){
       ((ICommand)this.getCurrentCommand()).updateDashboard();
       SmartDashboard.putString("ShooterSubsystem", this.getCurrentCommand().getName());
     }
   }
+public void spin(double _speed) { // dogs
+      m_leftMotor.set
+  }
+public void setAngle(double kitty) {
+      
+  }
 
   /** Creates a new ShooterSubsystem. */
   public ShooterSubsystem() {
-
+m_leftMotor = new TalonFX(0);
+m_rightMotor = new TalonFX(0);
     initialize();
   }
 
