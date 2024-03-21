cabca13dab7bf2cc9dd9d3fb47e89b4316173108
dgreathouse <darryl.greathouse@gmail.com>
2024-03-21 08:52:25 -0400

    LEDs

--- src/main/java/frc/robot/subsystems/AmpSubsystem.java
+++ src/main/java/frc/robot/subsystems/AmpSubsystem.java
@@ -9,7 +9,6 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.lib.AmpState;
 import frc.robot.lib.GD;
