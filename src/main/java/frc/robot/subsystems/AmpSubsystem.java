a47d3165c4200a0e35a81a6bb28e6f8f85e13183
Darryl <darryl.greathouse@gmail.com>
2024-03-21 14:41:52 -0400

    Amp Dashboard

--- src/main/java/frc/robot/subsystems/AmpSubsystem.java
+++ src/main/java/frc/robot/subsystems/AmpSubsystem.java
@@ -9,6 +9,7 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.lib.AmpState;
 import frc.robot.lib.GD;
